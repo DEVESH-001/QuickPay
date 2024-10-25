3f9e89eb1fe5ee9afc5071b805689ca4c5ad7ab8
DEVESH-001 <123268114+DEVESH-001@users.noreply.github.com>
2024-10-25 21:39:26 +0530

    Update index.js
    added corn

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,14 @@ const accountRouter = require("./routes/account"); // Import account routes
 
 const app = express();
 
-app.use(cors());
+
+app.use(cors(
+  {
+    origin:["https://quick-pay-nine.vercel.app"],
+    methods:["POST","GET"],
+    credentials:true
+  }
+));
 app.use(express.json());
 
 // Register your routes
